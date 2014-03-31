89ba7b016fac38711e01f4a250d52de9a3a495b8
Felipe Araújo Nunes de Lima <felipew3c@hotmail.com.br>
2014-03-31 19:10:14 -0300

    Update quest.php
    Ajuste das consultas sql, construção das funções

--- quest.php
+++ quest.php
@@ -17,22 +17,45 @@
 			public $enunciado;
 			public $resposta;
 
-			function __constructor(){
-				$c = pg_connect("")
-				$query = "select id,enunciado,resposta form quest order by rand";
-				$r = mysql_query($query);
-				$this->id = mysql_result($r, 0, 'id');
-				$this->enunciado = mysql_result($r, 0, 'enunciado');
-				$this->resposta = mysql_result($r, 0, 'resposta');
+			function __construct(){
+				
+				$c=pg_connect ("host='localhost' dbname='gincana' user='postgres' password='m2smart'");
+				$r = pg_execute($c,$query);
+				$r = pg_query($c, "SELECT id,enunciado,resposta FROM quest ORDER BY random() limit 1000");
+
+				$q = @pg_fetch_array($r, 0);
+				$this->id = $q['id'];
+				$this->enunciado = $q['enunciado'];
+				$this->resposta = $q['resposta'];
 			}
 
 			function show(){
 				?><h3>
 				<?php echo $this->enunciado; ?>
 				</h3>
-				<form method='get'>
+				<script type="text/javascript">
+
+					function showR(){
+						document.getElementById("resposta-certa").innerHTML = "<?php echo $this->resposta; ?>";
+						// document.getElementById("buttons").innerHTML = "<input type='submit' value='Acertei'><input type='submit' value='Errei'>";
+						document.getElementById("button-yes").innerHTML = "<input type='submit' value='Acertei'>";
+						document.getElementById("button-no").innerHTML = "<input type='submit' value='Errei'>";
+					}
+
+				</script>
+				
 					<input type='text' name='resp'>
-					<input type='submit' value='Responder'>
+					<button onclick="showR();">Responder</button><br>
+					<span id='resposta-certa'></span>
+				<form method='post'>
+					<input type='hidden' name='id' value='<?php echo $this->id ;?>'>
+					<input type='hidden' name='status' value='1'>
+					<div id='button-yes'></div>
+				</form>
+				<form method='post'>
+					<input type='hidden' name='id' value='<?php echo $this->id ;?>'>
+					<input type='hidden' name='status' value='0'>
+					<div id='button-no'></div>
 				</form>
 				<?php
 			}
@@ -43,8 +66,6 @@
 		$p = new Pergunta();
 
 		$p->show();
-
-		echo "123";
 
 	 ?>
 </body>
